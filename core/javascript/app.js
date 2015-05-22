1d4d33225fd8fa1de1dbceedd152f0ec34a05049
Cretection <info@cretection.eu>
2015-05-22 13:34:14 +0200

    Release 0.0.2

--- core/javascript/app.js
+++ core/javascript/app.js
@@ -1,62 +1,9 @@
-/**
-* Create couple of jQuery Deferred Objects to catch the 
-* firing of the two events associated with the loading of
-* the two frameworks.
+/*
+Project: org.rettapp.aid
+Author: Jonathan Starck
+License: GNU GENERAL PUBLIC Version2, June 1991 & European Union Public Licence
+Source: https://github.com/RettApp/org.rettapp.aid
+
+Last Updated: 17. Mai 2015 22:47:42
 */
-var versionRettApp = "001";
-var gapReady = $.Deferred();
-var jqmReady = $.Deferred();
-
-//Catch "deviceready" event which is fired when PhoneGap is ready
-document.addEventListener("deviceReady", deviceReady, false);
-
-//Resolve gapReady in reponse to deviceReady event
-function deviceReady()
-{
-	gapReady.resolve();
-}
-
-/**
-* Catch "mobileinit" event which is fired when a jQueryMobile is loaded.
-* Ensure that we respond to this event only once.
-*/
-$(document).one("mobileinit", function(){
-	jqmReady.resolve();
-});
-
-/**
-* Run your App Logic only when both frameworks have loaded
-*/
-$.when(gapReady, jqmReady).then(myAppLogic);
-
-// App Logic
-function myAppLogic(){
-	var obj = $('a[data-rel="external"]');
-	$.each(obj, function(){
-		currentLink = $(this).attr('href');
-		$(this).attr('href', '#');
-		$(this).attr('onclick', 'openDeviceBrowser("'+currentLink+'")' );
-	});
-	$('a[data-func="vibrate"]').click(function(){
-		navigator.notification.vibrate(100);
-	});
-	$(document).on("pagebeforeshow", "#main-02-index", function(event) {
-		$.ajax({
-			dataType: 'jsonp',
-			jsonp: 'callback',
-			url: 'http://aid.rettapp.org/version.php',
-			timeout: 2000,
-			error: function(){
-				$('.showVersion').html('<a href="#" class="ui-btn ui-corner-all ui-btn-icon-right ui-icon-alert ui-shadow ui-state-disabled">Versionsabfrage nicht möglich!</a>');
-			},
-			success: function (data, textStatus) {
-				if(versionRettApp >= data.version){
-					$('.showVersion').html('<a href="#" class="ui-btn ui-corner-all ui-btn-icon-right ui-icon-check ui-shadow ui-state-disabled">Neueste Version installiert</a>');
-				}else{
-					$('.showVersion').html('<a href="#" onclick="openDeviceBrowser("http://aid.rettapp.org/");" class="ui-btn ui-corner-all ui-btn-icon-right ui-icon-alert ui-shadow">Aktueller Version verfügbar</a>');
-				}
-			}
-		});
-    });
-}
-
+var versionRettApp="001";var gapReady=$.Deferred();var jqmReady=$.Deferred();document.addEventListener("deviceReady",deviceReady,false);function deviceReady(){gapReady.resolve()}$(document).one("mobileinit",function(){jqmReady.resolve()});$.when(gapReady,jqmReady).then(myAppLogic);function myAppLogic(){var a=$('a[data-rel="external"]');$.each(a,function(){currentLink=$(this).attr("href");$(this).attr("href","#");$(this).attr("onclick",'openDeviceBrowser("'+currentLink+'")')});$('a[data-func="vibrate"]').click(function(){navigator.notification.vibrate(100)});$(document).on("pagebeforeshow","#main-02-index",function(b){$.ajax({dataType:"jsonp",jsonp:"callback",url:"http://aid.rettapp.org/version.php",timeout:2000,error:function(){$(".showVersion").html('<a href="#" class="ui-btn ui-corner-all ui-btn-icon-right ui-icon-alert ui-shadow ui-state-disabled">Versionsabfrage nicht möglich!</a>')},success:function(c,d){if(versionRettApp>=c.version){$(".showVersion").html('<a href="#" class="ui-btn ui-corner-all ui-btn-icon-right ui-icon-check ui-shadow ui-state-disabled">Neueste Version installiert</a>')}else{$(".showVersion").html('<a href="#" onclick="openDeviceBrowser("http://aid.rettapp.org/");" class="ui-btn ui-corner-all ui-btn-icon-right ui-icon-alert ui-shadow">Aktueller Version verfügbar</a>')}}})})};
--- core/javascript/custom.js
+++ core/javascript/custom.js
@@ -1,51 +1,9 @@
-if (localStorage.getItem("settings") === null) {
-	var settings = {};
-	settings.startdisclaimer = '0';
-	localStorage.setItem("settings", JSON.stringify(settings));
-};
-function panelAndListRefresh() {
-	$('body > [data-role="panel"]').panel();
-	$('body > [data-role="panel"] [data-role="listview"]').listview().listview('refresh');
-	$('body > [data-role="page"] [data-role="listview"]').listview().listview('refresh');
-}
-function changeSliderOnSettings(sliderID, sliderIDvalue) {
-	$(sliderID+' [value="'+sliderIDvalue+'"]').prop("selected", true);
-	$(sliderID).slider().slider("refresh");
-}
-$(document).on("pageinit", function(event) {
-	panelAndListRefresh();
-});
-$(document).on("pagebeforecreate", "#main-01-disclaimer", function(event, ui) {
-	var settings = JSON.parse(localStorage.getItem('settings'));
-	if(settings.startdisclaimer == "1") {
-		$.mobile.changePage("#main-02-index");
-	};
-});
+/*
+Project: org.rettapp.aid
+Author: Jonathan Starck
+License: GNU GENERAL PUBLIC Version2, June 1991 & European Union Public Licence
+Source: https://github.com/RettApp/org.rettapp.aid
 
-
-$(document).on("pagebeforeshow", "#function-02-settings", function(event) {
-	var settings = JSON.parse(localStorage.getItem('settings'));
-	changeSliderOnSettings('#settings-startdisclaimer', settings.startdisclaimer);
-});
-
-
-
-$(document).on("pageshow", function(event) {
-	$("#saveSettings").click(function(){
-		var settings = {};
-		settings.startdisclaimer = $('#settings-startdisclaimer').val();
-		localStorage.setItem("settings", JSON.stringify(settings));
-		location.reload(true);
-	});
-});
-
-
-$(document).on("pageinit", function(){
-		$(".aid").listview({
-		    autodividers: true,
-		    autodividersSelector: function (li) {
-		        var out = li.attr('divider');
-		        return out;
-		    }
-		}).listview('refresh');
-	});
+Last Updated: 22. Mai 2015 13:19:59
+*/
+if(localStorage.getItem("settings")===null){var settings={};settings.startdisclaimer="0";localStorage.setItem("settings",JSON.stringify(settings))}function panelAndListRefresh(){$('body > [data-role="panel"]').panel();$('body > [data-role="panel"] [data-role="listview"]').listview().listview("refresh");$('body > [data-role="page"] [data-role="listview"]').listview().listview("refresh")}function changeSliderOnSettings(a,b){$(a+' [value="'+b+'"]').prop("selected",true);$(a).slider().slider("refresh")}$(document).on("pageinit",function(a){panelAndListRefresh()});$(document).on("pagebeforecreate","#main-01-disclaimer",function(b,c){var a=JSON.parse(localStorage.getItem("settings"));if(a.startdisclaimer=="1"){$.mobile.changePage("#main-02-index")}});$(document).on("pagebeforeshow","#function-02-settings",function(b){var a=JSON.parse(localStorage.getItem("settings"));changeSliderOnSettings("#settings-startdisclaimer",a.startdisclaimer)});$(document).on("pageshow",function(a){$("#saveSettings").click(function(){var b={};b.startdisclaimer=$("#settings-startdisclaimer").val();localStorage.setItem("settings",JSON.stringify(b));location.reload(true)})});$(document).on("pageshow","#aid-26",function(a){$("input").change(function(){unityLabel=$("#unity :radio:checked").attr("label");unityValue=$("#unity :radio:checked").val();agentAmount=$("#agent-amount").val();liquidAmount=$("#liquid-amount").val();unityPerKg=$("#unity-per-kg").val();patientWeight=$("#patient-weight").val();if(unityValue==="1"){results="<b>"+agentAmount+unityLabel+" auf "+liquidAmount+" ml</b><br>";results+=agentAmount*1000+"mg auf "+liquidAmount+" ml<br>";results+=agentAmount*1000000+"µg auf "+liquidAmount+" ml";$("#doseResultsConditionDefinition").html(results)}else{if(unityValue==="1000"){results=agentAmount/1000+"g auf "+liquidAmount+" ml</b><br>";results+="<b>"+agentAmount+unityLabel+" auf "+liquidAmount+" ml</b><br>";results+=agentAmount*1000+"µg auf "+liquidAmount+" ml";$("#doseResultsConditionDefinition").html(results)}else{if(unityValue==="1000000"){results=agentAmount/1000000+"g auf "+liquidAmount+" ml</b><br>";results+=agentAmount/1000+"mg auf "+liquidAmount+" ml<br>";results+="<b>"+agentAmount+unityLabel+" auf "+liquidAmount+" ml<b>";$("#doseResultsConditionDefinition").html(results)}}}if(unityValue==="1"){results="<b>"+agentAmount/liquidAmount+" "+unityLabel+"/ml</b><br>";results+=agentAmount*1000/liquidAmount+" mg/ml<br>";results+=agentAmount*1000000/liquidAmount+" µg/ml";$("#doseResultsCorrespondValue").html(results)}else{if(unityValue==="1000"){results=agentAmount/1000/liquidAmount+" g/ml<br>";results+="<b>"+agentAmount/liquidAmount+" "+unityLabel+"/ml</b><br>";results+=agentAmount*1000/liquidAmount+" µg/ml";$("#doseResultsCorrespondValue").html(results)}else{if(unityValue==="1000000"){results=agentAmount/1000000/liquidAmount+" g/ml<br>";results+=agentAmount/1000/liquidAmount+" mg/ml<br>";results+="<b>"+agentAmount/liquidAmount+" "+unityLabel+"/ml</b>";$("#doseResultsCorrespondValue").html(results)}}}if(unityValue==="1"){correspond=agentAmount*1000/liquidAmount;results=patientWeight*unityPerKg+" mg<br>";results+=patientWeight*unityPerKg/correspond+" ml bei aktueller Dosis";$("#doseResultsDoseValue").html(results)}else{if(unityValue==="1000"){correspond=agentAmount/liquidAmount;results=patientWeight*unityPerKg+" mg<br>";results+=patientWeight*unityPerKg/correspond+" ml bei aktueller Dosis";$("#doseResultsDoseValue").html(results)}else{if(unityValue==="1000000"){correspond=agentAmount/1000/liquidAmount;results=patientWeight*unityPerKg+" mg<br>";results+=patientWeight*unityPerKg/correspond+" ml bei aktueller Dosis";$("#doseResultsDoseValue").html(results)}}}})});$(document).on("pageinit",function(){$(".aid").listview({autodividers:true,autodividersSelector:function(a){var b=a.attr("divider");return b}}).listview("refresh")});
