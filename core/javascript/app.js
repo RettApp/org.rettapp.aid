a372f381748ba89964ac602bed72dfa36449a726
Cretection <info@cretection.eu>
2015-05-29 11:56:54 +0200

    Version

--- core/javascript/app.js
+++ core/javascript/app.js
@@ -1,30 +1,9 @@
-/**
-* Create couple of jQuery Deferred Objects to catch the 
-* firing of the two events associated with the loading of
-* the two frameworks.
+/*
+Project: org.rettapp.aid
+Author: Jonathan Starck
+License: GNU GENERAL PUBLIC Version2, June 1991 & European Union Public Licence
+Source: https://github.com/RettApp/org.rettapp.aid
+
+Last Updated: 29. Mai 2015 11:03:02
 */
-var versionRettApp = "001";
-var jqmReady = $.Deferred(), pgReady = $.Deferred();
-
-document.addEventListener("deviceready", phonegapReady(), false);
-
-function phonegapReady(){
-	$(document).on( "mobileinit", function(){
-		myAppLogic();
-	});
-}
-
-// App Logic
-function myAppLogic(){
-	$(document).on('click', 'a', function (e){
-		var elem = $(this);
-		var url = elem.attr('href');
-		if (elem.attr('data-rel') == 'external') {
-			e.preventDefault();
-			window.open(url, '_system');
-			return false;
-		}
-	})
-}
-
-
+var versionRettApp="001";var jqmReady=$.Deferred(),pgReady=$.Deferred();document.addEventListener("deviceready",phonegapReady(),false);function phonegapReady(){$(document).on("mobileinit",function(){myAppLogic()})}function myAppLogic(){$(document).on("click","a",function(c){var b=$(this);var a=b.attr("href");if(b.attr("data-rel")=="external"){c.preventDefault();window.open(a,"_system");return false}})};
